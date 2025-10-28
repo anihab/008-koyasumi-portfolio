c9c193cb87e99fc696f3bfa633d9e3706456f9ee
anisa <74878427+anihab@users.noreply.github.com>
2025-10-27 18:06:33 -0600

    Fix: remove conflicting ImageSharp typings to resolve Netlify build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,7 @@
+// Keep schema customization minimal to avoid conflicts with Gatsby's ImageSharp types.
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
   const typeDefs = `
-    type File implements Node {
-      childImageSharp: ImageSharp
-    }
-
-    type ImageSharp implements Node {
-      fluid: ImageSharpFluid
-      fixed: ImageSharpFixed
-    }
-
-    type ImageSharpFluid {
-      base64: String
-      aspectRatio: Float
-      src: String
-      srcSet: String
-      sizes: String
-    }
-
-    type ImageSharpFixed {
-      base64: String
-      width: Int
-      height: Int
-      src: String
-      srcSet: String
-    }
-    
-    type MarkdownRemark implements Node {
-      fileAbsolutePath: String
-      frontmatter: MarkdownRemarkFrontmatter
-    }
-
     type MarkdownRemarkFrontmatter {
       title: String
       image: String
